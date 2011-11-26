24cbf9bcdf1fff6ee86f4be4b3d66c823c75eae6
Markus Lorch <m.lorch@it-kult.de>
2011-11-26 12:42:16 +0100

    Inital Upload

---
+++ config.inc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Facebook API - config.inc.php
+ * @author m[dot]lorch[at]it-kult[dot]de Markus Lorch
+ * @package redaxo4
+ * @version 3.1.1
+ */
+
+/*
+ini_set('error_reporting', 'E_ALL');
+ini_set('display_errors', 'On');
+*/
+
+$mypage = "facebook_sdk";
+//$REX['ADDON']['name'][$mypage] = 'Facebook API';
+$REX['ADDON']['version'][$mypage] = '3.1.0';
+$REX['ADDON']['author'][$mypage] = 'Markus Lorch / ITKULT';
+$REX['ADDON']['supportpage'][$mypage] = 'http://www.it-kult.de';
+$REX['ADDON']['basedir'][$mypage] = dirname(__FILE__);
+
+$REX['PERM'][] = $mypage.'[]';
+
+## Include Class
+require_once $REX['ADDON']['basedir'][$mypage].'/classes/facebook.php';
+
+/*
+//
+// Los geht's: Die Daten Deiner Facebook App
+// http://developers.facebook.com
+//
+$fbconf['appId'] = '';
+$fbconf['secret'] = '';
+$fbconf['fileUploadSupport'] = true;
+
+// Initalisieren der Facebook-Klasse
+$facebook = new Facebook($fbconf);
+*/
+?>
---
+++ uninstall.inc.php
@@ -0,0 +1,3 @@
+<?php
+$REX['ADDON']['install']['facebook_sdk'] = 0;
+?>
